2b8eef59caec33262015c82be7e1bb88067f5501
mtobeery <64616989+mtobeery@users.noreply.github.com>
2025-07-30 22:51:41 +0300

    Support arithmetic expressions

--- graceb/src/parser.c
+++ graceb/src/parser.c
@@ -18,6 +18,43 @@ static Token* advance() {
     return &tokens[current++];
 }
 
+static ASTNode* parse_primary() {
+    Token* t = advance();
+    ASTNode* node = malloc(sizeof(ASTNode));
+    memset(node, 0, sizeof(ASTNode));
+
+    if (t->type == TOKEN_NUMBER) {
+        node->type = AST_INT_LITERAL;
+        node->value = strdup(t->lexeme);
+    } else if (t->type == TOKEN_IDENTIFIER) {
+        node->type = AST_IDENTIFIER;
+        node->name = strdup(t->lexeme);
+    } else {
+        free(node);
+        return NULL;
+    }
+
+    return node;
+}
+
+static ASTNode* parse_expression() {
+    ASTNode* left = parse_primary();
+    while (peek()->type == TOKEN_PUNCTUATION &&
+           (strcmp(peek()->lexeme, "+") == 0 || strcmp(peek()->lexeme, "-") == 0)) {
+        char op = peek()->lexeme[0];
+        advance();
+        ASTNode* right = parse_primary();
+        ASTNode* expr = malloc(sizeof(ASTNode));
+        memset(expr, 0, sizeof(ASTNode));
+        expr->type = AST_BINARY_EXPR;
+        expr->op = op;
+        expr->left = left;
+        expr->right = right;
+        left = expr;
+    }
+    return left;
+}
+
 ASTNode* parse_tokens() {
     ASTNode* head = NULL;
     ASTNode* tail = NULL;
@@ -28,12 +65,18 @@ ASTNode* parse_tokens() {
         if (t->type == TOKEN_INT) {
             Token* var = advance();
             advance();
-            Token* val = advance();
+            ASTNode* expr = parse_expression();
+
+            int result = evaluate(expr);
+            char buffer[64];
+            snprintf(buffer, sizeof(buffer), "%d", result);
 
             ASTNode* node = malloc(sizeof(ASTNode));
+            memset(node, 0, sizeof(ASTNode));
             node->type = AST_VAR_DECL;
             node->name = strdup(var->lexeme);
-            node->value = strdup(val->lexeme);
+            node->value = strdup(buffer);
+            node->left = expr;
             node->next = NULL;
 
             add_symbol(node->name, node->value);
@@ -47,6 +90,7 @@ ASTNode* parse_tokens() {
         if (strcmp(t->lexeme, "print") == 0) {
             Token* next = advance();
             ASTNode* node = malloc(sizeof(ASTNode));
+            memset(node, 0, sizeof(ASTNode));
             node->type = AST_PRINT_STATEMENT;
             node->name = strdup(next->lexeme);
             node->value = NULL;
